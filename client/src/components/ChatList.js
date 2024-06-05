70fa1818e3e09608996266b02155db2613f48c4a
Bilge <be@meo.io>
2024-06-05 15:59:01 +0100

    Added scroll library

--- client/src/components/ChatList.js
+++ client/src/components/ChatList.js
@@ -2,15 +2,17 @@ import styles from "./styles.module.css";
 import { useChat } from "../context/ChatContext";
 import ChatItem from "./ChatItem";
 
+import ScrollableFeed from 'react-scrollable-feed';
+
 function ChatList() {
   const { messages } = useChat();
   return (
     <div className={styles.chatList}>
-      <div>
+      <ScrollableFeed forceScroll="true">
         {messages.map((item, key) => (
           <ChatItem key={key} item={item} />
         ))}
-      </div>
+      </ScrollableFeed>
     </div>
   );
 }
